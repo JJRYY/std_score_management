31e57f7afa5e7f991432aac706eb403c5e92154a
Ryong <kjr2436@gmail.com>
2021-03-20 14:33:30 +0900

    view 수정

--- Scripts/3.view.sql
+++ Scripts/3.view.sql
@@ -7,8 +7,12 @@ select s.stdNo, stdName, banCode,
 	sum(if(subjectCode = 201, stdScore, 0)) as eng,
 	sum(if(subjectCode = 301, stdScore, 0)) as math,
 	sum(if(subjectCode = 401, stdScore, 0)) as soc,
-	sum(if(subjectCode = 501, stdScore, 0)) as sci
+	sum(if(subjectCode = 501, stdScore, 0)) as sci,
+	sum(stdScore) as '총점',
+	avg(stdScore) as '평균'
 	from student s join score c on s.stdNo = c.stdNo
 	group by stdNo;
 	
+select * from vw_student_score;
+
 select stdNo, stdName, banCode, kor, eng, math, soc, sci from vw_student_score;
--- Scripts/4.test.sql
+++ Scripts/4.test.sql
@@ -58,6 +58,17 @@ select stdNo, gender, enterDate, stdPhoto from std_detail;
 
 delete from std_detail where stdNo = 20001;
 	
+select s.stdNo, stdName, banCode,  
+	sum(if(subjectCode = 101, stdScore, 0)) as kor,
+	sum(if(subjectCode = 201, stdScore, 0)) as eng,
+	sum(if(subjectCode = 301, stdScore, 0)) as math,
+	sum(if(subjectCode = 401, stdScore, 0)) as soc,
+	sum(if(subjectCode = 501, stdScore, 0)) as sci,
+	sum(stdScore) as '총점',
+	avg(stdScore) as '평균'
+	from student s join score c on s.stdNo = c.stdNo
+	group by stdNo;
+
 -- 상위 몇개만 가져오기
 select * from vw_student_score;
 -- 과목점수 상위 5개만 가져오기
