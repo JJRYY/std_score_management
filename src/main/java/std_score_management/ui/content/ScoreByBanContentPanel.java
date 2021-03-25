32e3415bcb7da833306093bf50ae0f3dcf597076
Ryong <kjr2436@gmail.com>
2021-03-25 17:47:01 +0900

    수정중

---
+++ src/main/java/std_score_management/ui/content/ScoreByBanContentPanel.java
@@ -0,0 +1,12 @@
+package std_score_management.ui.content;
+
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class ScoreByBanContentPanel extends JPanel {
+
+	public ScoreByBanContentPanel() {
+
+	}
+
+}
