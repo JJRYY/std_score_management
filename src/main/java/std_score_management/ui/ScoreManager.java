8d687af735638975cca8b6217bd13b5d4ce9c1f4
Ryong <kjr2436@gmail.com>
2021-03-29 11:45:04 +0900

    수정

--- src/main/java/std_score_management/ui/ScoreManager.java
+++ src/main/java/std_score_management/ui/ScoreManager.java
@@ -46,7 +46,7 @@ public class ScoreManager extends JFrame implements ActionListener {
 	private void initialize() {
 		setTitle("성적 관리");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(150, 150, 630, 350);
+		setBounds(150, 150, 630, 320);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
